371e7df7c9e85dfaac6bc67574ab5a2b6c97dce0
Hanley Yunanda <hanleyunanda25@gmail.com>
2025-12-20 11:52:06 +0700

    init project fintar

---
+++ src/main/java/com/example/fintar/FintarApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fintar/FintarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
